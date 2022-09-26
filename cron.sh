086d830e5657b0780ea0be8afe307badc8ce0d92
cedric <cedric@saooti.com>
2022-09-26 16:32:36 +0200

    Script cron pour la génération automatique

---
+++ cron.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+PID=$(pgrep gnome-session)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+
+/usr/local/bin/python /home/cedric/.gnome-wallpaper/cron.py
